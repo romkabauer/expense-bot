a212917c8c801b655210926758bc0b07ca41488a
romkabauer <pm.rvbauer@gmail.com>
2023-10-16 23:18:09 +0200

    refactoring of work with the mapping

--- handlers/base_handler.py
+++ handlers/base_handler.py
@@ -1,5 +1,8 @@
-from aiogram import Bot
 from abc import abstractmethod
+from aiogram import Bot, types
+from aiogram.dispatcher import FSMContext
+import requests as r
+import json
 
 from resources.states import States
 from logger import Logger
@@ -15,3 +18,79 @@ class BaseHandler:
     @abstractmethod
     async def __call__(self, *args, **kwargs):
         pass
+
+    @staticmethod
+    async def save_init_instruction_msg_id(msg: types.Message, state: FSMContext):
+        async with state.proxy() as data:
+            data['init_instruction'] = msg.message_id
+
+    async def delete_init_instruction(self, chat_id: int, state: FSMContext):
+        async with state.proxy() as data:
+            init_msg_id = data['init_instruction']
+        await self.bot.delete_message(chat_id=chat_id, message_id=init_msg_id)
+
+    @staticmethod
+    async def __fill_payload_field_date(field_key: str, value: str, state: FSMContext):
+        date_parts = value.split("-")
+        async with state.proxy() as data:
+            data["form_payload"][f"{field_key}_year"] = date_parts[0]
+            data["form_payload"][f"{field_key}_month"] = date_parts[1]
+            data["form_payload"][f"{field_key}_day"] = date_parts[2]
+
+    @staticmethod
+    async def __fill_payload_field_short_text(field_key: str, value: str | int, state: FSMContext):
+        async with state.proxy() as data:
+            data["form_payload"][field_key] = value
+
+    @staticmethod
+    async def __fill_payload_field_soft_single_choice(field_key: str, value: str, state: FSMContext):
+        async with state.proxy() as data:
+            data["form_payload"][field_key] = "__other_option__"
+            data["form_payload"][f"{field_key}.other_option_response"] = value
+
+    async def fill_payload_field(self, field_key: str, value: str | int, state: FSMContext):
+        async with state.proxy() as data:
+            if not data.get("form_payload"):
+                data["form_payload"] = {}
+        field_properties = self.config.get("form_payload_mapping").get(field_key)
+        match field_properties["type"]:
+            case "date":
+                await self.__fill_payload_field_date(field_properties["key"], value, state)
+            case "soft_single_choice":
+                await self.__fill_payload_field_soft_single_choice(field_properties["key"], value, state)
+            case _:
+                await self.__fill_payload_field_short_text(field_properties["key"], value, state)
+
+    async def prettify_form_response(self, form_response: dict):
+        prettified_response = {}
+        self.logger.info(self, "", f"Original payload: {form_response}")
+        mapping = self.config.get("form_payload_mapping")
+        for key in mapping.keys():
+            match mapping[key]["type"]:
+                case "date":
+                    prettified_response[key] = form_response.get(f"{mapping[key]['key']}_year") + '-' + \
+                                               form_response.get(f"{mapping[key]['key']}_month") + '-' + \
+                                               form_response.get(f"{mapping[key]['key']}_day")
+                case "soft_single_choice":
+                    prettified_response[key] = form_response.get(f"{mapping[key]['key']}.other_option_response")
+                case _:
+                    prettified_response[key] = form_response.get(mapping[key]["key"])
+        return prettified_response
+
+    async def send_form_response(self, chat_id: int, state: FSMContext):
+        async with state.proxy() as data:
+            res = r.post(self.config.get("form_url"), data=data['form_payload'])
+            form_payload = await self.prettify_form_response(data['form_payload'])
+
+        if res.status_code == 200:
+            await self.bot.send_message(chat_id=chat_id,
+                                        text=f"✅Expense has been recorded!\n"
+                                             f"Recorded data: \n"
+                                             f"{json.dumps(form_payload, sort_keys=True, indent=4)}",
+                                        disable_notification=True)
+        else:
+            await self.bot.send_message(chat_id=chat_id,
+                                        text=f"⛔️NOT recorded!\n"
+                                             f"Data to be recorded: \n"
+                                             f"{json.dumps(form_payload, sort_keys=True, indent=4)}",
+                                        disable_notification=True)
--- handlers/expenses.py
+++ handlers/expenses.py
@@ -1,13 +1,10 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-import json
 import re
-import requests as r
 from datetime import datetime as dt, timedelta as td
 
 from handlers.base_handler import BaseHandler
 from resources.keyboards import build_date_keyboard, build_listlike_keyboard
-from resources.form_sender import get_default_form_payload
 
 
 class StartAddingExpense(BaseHandler):
@@ -16,8 +13,7 @@ class StartAddingExpense(BaseHandler):
                                   reply_markup=build_date_keyboard(),
                                   reply=False,
                                   disable_notification=True)
-        async with state.proxy() as data:
-            data['init_instruction'] = msg["message_id"]
+        await self.save_init_instruction_msg_id(msg, state)
         await message.delete()
 
 
@@ -28,31 +24,20 @@ class InputDate(BaseHandler):
         if callback.data in ["today", "yesterday"]:
             msg = await callback.message.reply("🛍️What is the expense category?",
                                                reply_markup=build_listlike_keyboard(
-                                                   entities=self.config.get('spending_categories'),
+                                                   entities=self.config.get("spending_categories"),
                                                    max_items_in_a_row=3),
                                                reply=False,
                                                disable_notification=True)
-            async with state.proxy() as data:
-                data["init_instruction"] = msg["message_id"]
-                if not data.get("form_payload"):
-                    data["form_payload"] = get_default_form_payload(self.config.get("form_payload_template"))
+            await self.save_init_instruction_msg_id(msg, state)
 
-                year_key = [key for key in data["form_payload"].keys() if "year" in key][0]
-                month_key = [key for key in data["form_payload"].keys() if "month" in key][0]
-                day_key = [key for key in data["form_payload"].keys() if "day" in key][0]
-
-                data["form_payload"][year_key] = dt.now().year if callback.data == "today" \
-                    else (dt.now() - td(1)).year
-                data["form_payload"][month_key] = dt.now().month if callback.data == "today" \
-                    else (dt.now() - td(1)).month
-                data["form_payload"][day_key] = dt.now().day if callback.data == "today" \
-                    else (dt.now() - td(1)).day
+            when_value = dt.now().strftime('%Y-%m-%d') if callback.data == "today" \
+                else (dt.now() - td(1)).strftime('%Y-%m-%d')
+            await self.fill_payload_field(field_key="when", value=when_value, state=state)
         else:
             msg = await callback.message.reply("🔤Input expense date in format '2023-10-13':",
                                                reply=False,
                                                disable_notification=True)
-            async with state.proxy() as data:
-                data['init_instruction'] = msg["message_id"]
+            await self.save_init_instruction_msg_id(msg, state)
             await self.state.picking_day.set()
 
         await callback.message.delete()
@@ -61,52 +46,33 @@ class InputDate(BaseHandler):
 class ParseDate(BaseHandler):
     async def __call__(self, message: types.Message, state: FSMContext):
         await message.delete()
-        async with state.proxy() as data:
-            init_msg_to_delete = data.get('init_instruction')
 
         if not re.match(r"20\d{2}-(1[0-2]|0[1-9])-(3[0-1]|1[0-9]|2[0-9]|0[1-9])", message.text):
-            await self.bot.delete_message(chat_id=message.chat.id,
-                                          message_id=init_msg_to_delete)
-
+            await self.delete_init_instruction(message.chat.id, state)
             msg = await message.reply(text=f"⛔️Wrong date format.\n"
                                            f"🔤Input expense date in format '2023-10-13':",
                                       reply=False,
                                       disable_notification=True)
-            async with state.proxy() as data:
-                data['init_instruction'] = msg["message_id"]
+            await self.save_init_instruction_msg_id(msg, state)
             return
-        elif dt.strptime(message.text, '%Y-%m-%d') > dt.now():
-            await self.bot.delete_message(chat_id=message.chat.id,
-                                          message_id=init_msg_to_delete)
 
+        elif dt.strptime(message.text, '%Y-%m-%d') > dt.now():
+            await self.delete_init_instruction(message.chat.id, state)
             msg = await message.reply(text=f"⛔️Input cannot contain future dates.\n"
                                            f"🔤Input expense date in format '2023-10-13':",
                                       reply=False,
                                       disable_notification=True)
-            async with state.proxy() as data:
-                data['init_instruction'] = msg["message_id"]
+            await self.save_init_instruction_msg_id(msg, state)
             return
 
-        await self.bot.delete_message(chat_id=message.chat.id,
-                                      message_id=init_msg_to_delete)
+        await self.delete_init_instruction(message.chat.id, state)
 
         msg = await message.reply("🛍️What is the expense category?",
                                   reply_markup=build_listlike_keyboard(self.config.get('spending_categories')),
                                   reply=False,
                                   disable_notification=True)
-        async with state.proxy() as data:
-            data['init_instruction'] = msg["message_id"]
-            if not data.get('form_payload'):
-                data['form_payload'] = get_default_form_payload(self.config.get("form_payload_template"))
-            date_parts = message.text.split('-')
-
-            year_key = [key for key in data["form_payload"].keys() if "year" in key][0]
-            month_key = [key for key in data["form_payload"].keys() if "month" in key][0]
-            day_key = [key for key in data["form_payload"].keys() if "day" in key][0]
-
-            data["form_payload"][year_key] = date_parts[0]
-            data["form_payload"][month_key] = date_parts[1]
-            data["form_payload"][day_key] = date_parts[2]
+        await self.save_init_instruction_msg_id(msg, state)
+        await self.fill_payload_field(field_key="when", value=message.text, state=state)
 
 
 class AskAmount(BaseHandler):
@@ -114,111 +80,59 @@ class AskAmount(BaseHandler):
         await callback.answer()
         await callback.message.delete()
 
+        await self.fill_payload_field(field_key="category", value=callback.data, state=state)
+
         msg = await callback.message.reply("💵What is an amount paid?",
                                            reply=False,
                                            disable_notification=True)
-        async with state.proxy() as data:
-            data['init_instruction'] = msg["message_id"]
-            if not data.get('form_payload'):
-                data['form_payload'] = get_default_form_payload(self.config.get("form_payload_template"))
-
-            category_key = [key for key in data["form_payload"].keys()
-                            if key in self.config.get("form_payload_template").get("category").keys()
-                            and "category" in self.config.get("form_payload_template").get("category")[key]][0]
-            data['form_payload'][category_key] = callback.data
+        await self.save_init_instruction_msg_id(msg, state)
 
         await self.state.entering_amount.set()
 
 
 class ParseAmount(BaseHandler):
     async def __call__(self, message: types.Message, state: FSMContext):
-        async with state.proxy() as data:
-            init_msg_to_delete = data.get('init_instruction')
-
         await message.delete()
-        await self.bot.delete_message(chat_id=message.chat.id,
-                                      message_id=init_msg_to_delete)
+        await self.delete_init_instruction(chat_id=message.chat.id, state=state)
 
         if not re.match(r"\d+([.,]\d+)?", message.text):
             msg = await message.reply(text=f"⛔️Wrong format for spending amount.\n"
-                                           f"🔤Should contain only numbers possibly with . or , decimal separator:",
+                                           f"🔤Should contain only positive numbers "
+                                           f"possibly with . or , decimal separator:",
                                       reply=False,
                                       disable_notification=True)
-            async with state.proxy() as data:
-                data['init_instruction'] = msg["message_id"]
+            await self.save_init_instruction_msg_id(msg, state)
             return
 
         msg = await message.reply(text=f"🔤Choose any comment to add or choose 'Custom comment' and write custom one:",
                                   reply_markup=build_listlike_keyboard(self.config.get("comments")),
                                   reply=False,
                                   disable_notification=True)
-        async with state.proxy() as data:
-            data['init_instruction'] = msg["message_id"]
-            if not data.get('form_payload'):
-                data['form_payload'] = get_default_form_payload(self.config.get("form_payload_template"))
+        await self.save_init_instruction_msg_id(msg, state)
 
-            amount_key = [key for key in data["form_payload"].keys() if data["form_payload"][key] == 1000][0]
-            data['form_payload'][amount_key] = message.text
+        await self.fill_payload_field(field_key="amount", value=message.text, state=state)
 
 
 class InputComment(BaseHandler):
     async def __call__(self, callback: types.CallbackQuery, state: FSMContext):
-        form_payload = ""
-
         if callback.data == "Custom comment":
             msg = await callback.message.reply("🔤Input your comment:",
                                                reply=False,
                                                disable_notification=True)
+            await self.save_init_instruction_msg_id(msg, state)
             await callback.message.delete()
-            async with state.proxy() as data:
-                data['init_instruction'] = msg["message_id"]
             await self.state.commenting.set()
         else:
-            async with state.proxy() as data:
-                comment_key = [key for key in data["form_payload"].keys()
-                               if key in self.config.get("form_payload_template").get("comment").keys()][0]
-                data['form_payload'][comment_key] = callback.data
-                form_payload = data['form_payload']
-
-            res = r.post(self.config.get("form_url"), data=form_payload)
-            if res.status_code == 200:
-                await callback.message.reply(text=f"✅Expense has been recorded!\n"
-                                                  f"Recorded data: "
-                                                  f"{json.dumps(form_payload, sort_keys=True, indent=4)}",
-                                             disable_notification=True)
-            else:
-                await callback.message.reply(text=f"⛔️NOT recorded!\n"
-                                                  f"Data to be recorded: "
-                                                  f"{json.dumps(form_payload, sort_keys=True, indent=4)}",
-                                             disable_notification=True)
+            await self.fill_payload_field(field_key="comment", value=callback.data, state=state)
+            await self.send_form_response(chat_id=callback.message.chat.id, state=state)
             await callback.message.delete()
             await state.finish()
 
 
 class ParseComment(BaseHandler):
     async def __call__(self, message: types.Message, state: FSMContext):
-        async with state.proxy() as data:
-            init_msg_to_delete = data.get('init_instruction')
-        await self.bot.delete_message(chat_id=message.chat.id,
-                                      message_id=init_msg_to_delete)
-
-        async with state.proxy() as data:
-            comment_key = [key for key in data["form_payload"].keys()
-                           if key in self.config.get("form_payload_template").get("comment").keys()][0]
-            data['form_payload'][comment_key] = message.text
-            form_payload = data['form_payload']
-
-        res = r.post(self.config.get("form_url"), data=form_payload)
-        if res.status_code == 200:
-            await message.reply(text=f"✅Expense has been recorded!\n"
-                                     f"Recorded data: "
-                                     f"{json.dumps(form_payload, sort_keys=True, indent=4)}",
-                                disable_notification=True)
-        else:
-            await message.reply(text=f"⛔️NOT recorded!\n"
-                                     f"Data to be recorded: "
-                                     f"{json.dumps(form_payload, sort_keys=True, indent=4)}",
-                                disable_notification=True)
-
+        await self.delete_init_instruction(message.chat.id, state)
+        await self.fill_payload_field(field_key="comment", value=message.text, state=state)
+        await self.send_form_response(chat_id=message.chat.id, state=state)
         await message.delete()
         await state.finish()
