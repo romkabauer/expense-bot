c6af2fd0c80f58633da210dc9dd888bea622feb0
romkabauer <pm.rvbauer@gmail.com>
2024-08-20 09:15:19 +0300

    adjust user properties while eat out db upgrade

--- database/migrations/versions/cf76b988cebb_rename_eat_outside_to_eat_out.py
+++ database/migrations/versions/cf76b988cebb_rename_eat_outside_to_eat_out.py
@@ -23,6 +23,41 @@ def upgrade() -> None:
         UPDATE expense_bot.categories SET name = 'Eat Out'
         WHERE name = 'Eat outside'
     """)
+    op.execute("""
+        MERGE INTO expense_bot.users_properties AS t
+            USING (
+                select
+                  up.property_id,
+                  up.user_id,
+                  CASE
+                    WHEN jsonb(property_value) ? 'Eat outside'
+                      THEN jsonb(jsonb(property_value) - 'Eat outside' || jsonb_build_object('Eat Out', property_value->'Eat outside'))
+                    ELSE jsonb(property_value)
+                  END AS new_prop_value
+                from expense_bot.users_properties up
+                join expense_bot.properties p on up.property_id = p.id
+                where p.name IN ('amounts', 'comments')
+                
+                union all
+                
+                select
+                  property_id,
+                  user_id,
+                  CASE
+                    WHEN jsonb_array_length(to_jsonb(property_value) - 'Eat outside') <> jsonb_array_length(to_jsonb(property_value))
+                      THEN jsonb_insert(to_jsonb(property_value) - 'Eat outside', '{0}', '"Eat Out"')
+                    ELSE to_jsonb(property_value)
+                  END AS new_prop_value
+                from expense_bot.users_properties up
+                join expense_bot.properties p on up.property_id = p.id
+                where p.name = 'categories'
+            ) AS s
+        ON t.property_id = s.property_id AND t.user_id = s.user_id
+        WHEN MATCHED THEN
+          UPDATE SET
+            property_value = s.new_prop_value,
+            updated_at = CURRENT_TIMESTAMP
+    """)
 
 
 def downgrade() -> None:
@@ -30,3 +65,38 @@ def downgrade() -> None:
         UPDATE expense_bot.categories SET name = 'Eat outside'
         WHERE name = 'Eat Out'
     """)
+    op.execute("""
+            MERGE INTO expense_bot.users_properties AS t
+                USING (
+                    select
+                      up.property_id,
+                      up.user_id,
+                      CASE
+                        WHEN jsonb(property_value) ? 'Eat Out'
+                          THEN jsonb(jsonb(property_value) - 'Eat Out' || jsonb_build_object('Eat outside', property_value->'Eat Out'))
+                        ELSE jsonb(property_value)
+                      END AS new_prop_value
+                    from expense_bot.users_properties up
+                    join expense_bot.properties p on up.property_id = p.id
+                    where p.name IN ('amounts', 'comments')
+
+                    union all
+
+                    select
+                      property_id,
+                      user_id,
+                      CASE
+                        WHEN jsonb_array_length(to_jsonb(property_value) - 'Eat Out') <> jsonb_array_length(to_jsonb(property_value))
+                          THEN jsonb_insert(to_jsonb(property_value) - 'Eat Out', '{0}', '"Eat outside"')
+                        ELSE to_jsonb(property_value)
+                      END AS new_prop_value
+                    from expense_bot.users_properties up
+                    join expense_bot.properties p on up.property_id = p.id
+                    where p.name = 'categories'
+                ) AS s
+            ON t.property_id = s.property_id AND t.user_id = s.user_id
+            WHEN MATCHED THEN
+              UPDATE SET
+                property_value = s.new_prop_value,
+                updated_at = CURRENT_TIMESTAMP
+        """)
